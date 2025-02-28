a2562d94be20ece707cf7bcabd453d0be44da804
Eziocdl <eziocdl@gmail.com>
2025-02-28 15:32:58 -0300

    Implementa classe CalculadoraView com lógica CLI básica
    - Adiciona métodos para receber entrada do usuário (números e operação).
    - Adiciona métodos para exibir resultado e mensagens de erro.
    - Inicializa o Scanner no construtor.
    - Adiciona método para fechar o Scanner.

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_19
+    targetCompatibility = JavaVersion.VERSION_19
+}
+
+group = "io.github.eziocdl"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/view/CalculadoraView.java
@@ -0,0 +1,44 @@
+package view;
+
+
+import java.util.Scanner;
+
+public class CalculadoraView {
+
+
+    private Scanner scanner;
+
+    public CalculadoraView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+
+    // methods de entrada de dados
+
+public double receberNumero(String mensagem) {
+
+    System.out.println(mensagem);
+    return scanner.nextDouble();
+}
+
+public char receberOperacao() {
+    System.out.println("Digite a operação desejada(+, -, *, /):");
+    return scanner.next().charAt(0);
+
+}
+
+public void exibirResultado(double resultado) {
+
+        System.out.println("Resutltado: " + resultado);
+}
+
+public void exibirMensagemDeErro(String mensagem) {
+    System.out.println("Erro: " + mensagem);
+
+}
+
+public void fecharScanner() {
+        scanner.close();
+    }
+}
+
